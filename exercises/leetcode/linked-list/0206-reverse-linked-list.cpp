12b9b3b27d3fc0a03f07567b8c9d4b06f130df9c
JeanCassiano <jean_cassiano_@hotmail.com>
2026-05-24 11:07:11 +0200

    feat: added 026 reverse linked list

---
+++ exercises/leetcode/linked-list/0206-reverse-linked-list.cpp
@@ -0,0 +1,54 @@
+// Problem: Reverse Linked List
+// Link: https://leetcode.com/problems/reverse-linked-list/
+// Difficulty: Easy
+//
+// Approach:
+// We reverse the linked list iteratively using three pointers:
+//
+// - `prev` stores the previous node.
+// - `curr` stores the current node being processed.
+// - `next` temporarily stores the next node.
+//
+// For each node:
+//   - Save the next node.
+//   - Reverse the current node's pointer.
+//   - Move both `prev` and `curr` one step forward.
+//
+// At the end, `prev` becomes the new head of the reversed list.
+//
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        ListNode* next = nullptr;
+
+        while (curr != nullptr) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+};
